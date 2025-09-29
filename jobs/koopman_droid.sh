c7ccd470eec7d61df6c46b488bad864d4569ec56
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2025-09-30 00:33:43 +0300

    ADD: Job file for koopman droid training

---
+++ jobs/koopman_droid.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --account=project_2009050
+#SBATCH --job-name=vitg16_train
+#SBATCH --partition=gpu
+#SBATCH --time=5:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=320G
+#SBATCH --gres=gpu:v100:2                     
+#SBATCH --output=./logs/err_%j_%x_%N.out
+#SBATCH --error=./logs/err_%j_%x_%N.err
+
+
+
+
+# Load required modules
+module --force purge
+module load pytorch
+
+# Activate python environment
+source /scratch/project_2009050/torchy/bin/activate
+cd /projappl/project_2009050/KJEPA
+# git fetch
+# git pull
+
+# Launch with torchrun
+srun torchrun --standalone \
+  --nnodes=1 \
+  --nproc_per_node=2  \
+  app/main.py \
+  --fname configs/train/vitg16/koopman-droid-256px-8f.yaml
